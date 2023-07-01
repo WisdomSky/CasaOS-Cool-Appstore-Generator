36f5b7f429baa4c433322c2321caebbfcfbc0807
WisdomSky <i.wisdomsky@gmail.com>
2023-07-01 23:14:24 +0800

    fix missing tags for databases

--- src/apps/reactive-resume/config.js
+++ src/apps/reactive-resume/config.js
@@ -1,5 +1,12 @@
 export default {
     tag: {
         pattern: /^v\d+\.\d+\.\d+$/
+    },
+    services: {
+        db: {
+            tag: {
+                pattern: /^(\d+(\.\d+(\.\d)?)?)$/
+            }
+        }
     }
 }
